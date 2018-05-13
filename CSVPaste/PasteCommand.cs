020fab89bca3c742e5f0078fe175246027cc74a9
Timovski <stefan_timovski@outlook.com>
2018-05-13 20:31:19 +0200

    Implement dummy text insertion in current caret position

--- CSVPaste/PasteCommand.cs
+++ CSVPaste/PasteCommand.cs
@@ -93,17 +93,12 @@ namespace CSVPaste
         /// <param name="e">Event args.</param>
         private void MenuItemCallback(object sender, EventArgs e)
         {
-            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
-            string title = "PasteCommand";
+            var wpfTextView = WpfTextViewHelper.GetWpfTextView(ServiceProvider);
+            var caretPosition = WpfTextViewHelper.GetCaretPosition(wpfTextView);
 
-            // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox(
-                this.ServiceProvider,
-                message,
-                title,
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            var edit = wpfTextView.TextBuffer.CreateEdit();
+            edit.Insert(caretPosition, "dummy text...");
+            edit.Apply();
         }
     }
 }
